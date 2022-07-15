35e415e8bc57bedd71ce87066f89cbf99225eee0
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-07-15 16:56:38 +0530

    fine skills code

---
+++ database/user/index.js
@@ -0,0 +1,78 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+    },
+    usermail:{
+        type:String,
+        require:true,
+    },
+    usernumber:{
+        type:Number,
+        require:true,
+    },
+    userpassword:{
+        type:String,
+        require:true,
+    },
+    userconfirmpassword:{
+        type:String,
+        require:true,
+    },
+    tokens : [{
+        user__token:{
+            type : String,
+            required : true
+        }
+    }]
+
+});
+
+UserSchema.methods.generateAuthToken = async function(){
+    try {
+        const token = await jwt.sign({
+            _id : this._id.toString(),
+        },
+        "mynameistarachandkumawattaraisgo"
+        );
+        this.tokens = this.tokens.concat({user__token:token});
+        return token;
+    } catch (error) {
+        console.log("Token error is "+error);
+    }
+    
+}
+
+UserSchema.statics.userShouldNotExist = async (usermail) => {
+    const user = await UserModel.findOne({ usermail});
+    if (user) throw new Error("User Already exist.");
+    return false;
+  };
+  
+
+UserSchema.pre("save",function(next){
+    const user = this;
+    if(!user.isModified("userpassword")) return next();
+    bcrypt.genSalt(8,(err,salt)=>{
+        if(err) return next(err);
+        bcrypt.hash(user.userpassword,salt,(err,hash)=>{
+            if(err) return next(err);
+            user.userpassword = hash;
+            user.userconfirmpassword = undefined;
+            console.log(user.userpassword);
+            return next();
+        })
+    })
+});
+
+
+
+
+const UserModel = mongoose.model("users",UserSchema);
+
+module.exports = UserModel;
---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require("express");
+require("./database/conn");
+let UserModel = require("./database/user/index");
+const bcrypt = require("bcryptjs");
+const cors = require("cors");
+
+
+const PORT = process.env.PORT ||4000;
+
+
+const app = express();
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cors());
+
+app.get("/",(req,res)=>{
+    res.json({Message : "Success"});
+
+});
+
+app.post("/login",async(req,res)=>{
+    try {
+        const usermail = req.body.usermail;
+        const userpassword = req.body.userpassword;
+        const checkEmail = await UserModel.findOne({usermail});
+        if(checkEmail){
+            const checkPassword =  await bcrypt.compare(userpassword,checkEmail.userpassword);
+            console.log(checkPassword);
+            if(checkPassword){
+                const token = await checkEmail.generateAuthToken();
+                return res.status(200).json({token : token});
+            }
+            else{
+                return res.status(404).json({"error : " : "password does not match"});
+            }
+        }
+        else{
+            return res.status(404).json({"error : " : "email does not match"});
+        }
+        
+    } catch (error) {
+        return res.status(500).json({message : error});
+    }
+    
+
+});
+
+
+app.get("/list",async(req,res)=>{
+    // const token = localStorage.getItem('token');
+    console.log(token);
+    // let data = await UserModel.findOne({usermail:emailFind}); // complete database ka data milega
+    // console.log(data);
+    res.json(token);
+})
+
+app.post("/signup",async(req,res)=>{
+    try {
+        const userpassword = req.body.userpassword;
+        const usermail = req.body.usermail;
+        const userconfirmpassword= req.body.userconfirmpassword;
+        const chekMail = await UserModel.findOne({usermail});
+        console.log(chekMail);
+        
+        if(!chekMail){
+            console.log("user not found");
+            if(userpassword===userconfirmpassword){
+                console.log("password is same");
+                const userForm = new UserModel({
+                    username : req.body.username,
+                    usermail : req.body.usermail,
+                    usernumber : req.body.usernumber,
+                    userpassword:req.body.userpassword,
+                    userconfirmpassword : req.body.userconfirmpassword,
+
+                });
+                console.log("schema is created");
+                const token = await userForm.generateAuthToken();
+                const user = await userForm.save();
+                console.log(token);
+                console.log(user);
+                return res.status(200).json({
+                        user,token
+                });
+            }
+            else{
+                return res.status(202).json({"message" : "password does not match"});
+            }
+        }
+        else{
+            return res.status(404).json({error : "user already exist"});
+        }
+    } catch (error) {
+        return res.status(500).json({message : "catched error message"});
+    }
+    
+
+});
+
+
+if(process.env.NODE_ == 'production'){
+    app.use(express.static("client/build"));
+}
+
+app.listen(PORT,(error)=>{
+    if(!error){
+        console.log("server has been started on port 4000");
+    }
+    else{
+        console.log("error is "+ error);
+    }
+})
