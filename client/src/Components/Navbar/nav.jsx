d744699d5b05d07d1a268f38710cb0fe4c147c5a
Tara Chand Kumawat <tarachand122333@gmail.com>
2022-07-17 11:13:09 +0530

    changes navbar section in pc and tablet mode

--- client/src/Components/Navbar/nav.jsx
+++ client/src/Components/Navbar/nav.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {BiSearch} from 'react-icons/bi';
 import { NavLink } from "react-router-dom";
+import { SubNavbarSm } from "../SubNavbar/SubNavbar.component";
 
 
 const NavSm = ()=>{
@@ -14,18 +15,15 @@ const NavSm = ()=>{
 
     return(
         <>
-            <div className="flex justify-between items-center gap-3 p-3 md:p-4">
+            <div className="flex justify-between items-center gap-3 p-3 md:p-4 bg-tara-0 text-white">
                 <NavLink to="/">
                 <div className="flex gap-3 justify-center items-center">
-                    <div className="h-8 w-8 md:h-14 md:w-14">
-                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/2048px-Vanamo_Logo.svg.png" alt="logo" className="w-full h-full"/>
-                    </div>
                     <h3 className="font-bold text-xl md:text-3xl">FineSkills</h3>
                 </div>
                 </NavLink>
                 <div className="flex justify-center items-center">
                 <NavLink to="/login">
-                    <button className="bg-tara-0 text-white px-4 py-2 rounded-sm text-sm font-bold hover:bg-red-700">Login</button>
+                    <button className="bg-white text-tara-0 px-4 py-2 rounded-sm text-sm font-bold hover:bg-red-700">Login</button>
                     {/* <LoginComponent trigger={buttonPopup} setTrigger={setButtonPopup}>
                         <LoginPage />
                     </LoginComponent> */}
@@ -49,22 +47,18 @@ const NavLg = ()=>{
     return(
         <>
         <div className="w-full px-3 bg-tara-0 text-white">
-            <div className="flex justify-between gap-3 px-5 py-2 items-center mt-4">
+            <div className="flex justify-between gap-3 py-2 px-5 items-center">
                 <NavLink to="/">
                 <div className="flex items-center gap-1 cursor-pointer">
-                    <div className="h-8 w-8">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Vanamo_Logo.svg/2048px-Vanamo_Logo.svg.png" alt="logo" className="w-full h-full"/>
-                        </div>
-                        <h3 className="font-bold text-xl">FineSkills</h3> 
+                    <h3 className="font-bold text-xl">FineSkills</h3> 
                 </div>
                 </NavLink>
-                <div className="flex items-center w-1/2 px-2 rounded-sm bg-white" >
-                    <BiSearch className="text-xl font-bold cursor-not-allowed text-black" />
-                    <input type="search" placeholder="Explore" className="w-full px-3 py-1 bg-transparent focus:outline-none text-black font-bold"/>
+                <div>
+                    <SubNavbarSm />
                 </div>
                 <div className="flex items-center ">
                     <NavLink to="/login">
-                        <button className=" bg-white text-tara-0 text-sm px-6 py-1 rounded-sm font-bold hover:border-2 hover:border-white hover:bg-tara-0 hover:text-white">Login</button>
+                        <button className=" bg-white text-tara-0 text-sm border-2 border-tara-0 px-6 py-1 rounded-sm font-bold hover:border-2 hover:border-white hover:bg-tara-0 hover:text-white">Login</button>
                         {/* <LoginComponent trigger={buttonPopup} setTrigger={setButtonPopup}>
                             <LoginPage />
                         </LoginComponent> */}
--- client/src/Components/SubNavbar/SubNavbar.component.jsx
+++ client/src/Components/SubNavbar/SubNavbar.component.jsx
@@ -3,27 +3,27 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 let activeClassName = "underline";
 
-const SubNavbarSm = ()=>{
+export const SubNavbarSm = ()=>{
 
     
     return(
         <>
-            <div className="px-3 border-2 border-b-tara-0">
-            <div className="flex justify-center items-center md:gap-8">
-                    <div className="flex justify-between gap-1 px-2 py-2 items-center cursor-pointer hover:text-blue-800 font-bold">
+            <div className="px-3 border-b-tara-0 bg-tara-0 text-white">
+            <div className="flex justify-center items-center md:gap-6">
+                    <div className="flex justify-between gap-1 px-5 py-1 items-center cursor-pointer hover:bg-white hover:text-tara-0 font-bold">
                                 <NavLink to="/" className={({ isActive }) =>
                         isActive ? activeClassName : undefined
-                        }><h3 className="md:text-xl">Course</h3></NavLink>
+                        }><h3 className="md:text-base">Course</h3></NavLink>
                     </div>
-                    <div className="flex justify-between gap-1 px-2 py-2 items-center cursor-pointer hover:text-blue-800 font-bold">
+                    <div className="flex justify-between gap-1 px-5 py-1 items-center cursor-pointer hover:bg-white hover:text-tara-0 font-bold">
                                 <NavLink to="/profile" className={({ isActive }) =>
                         isActive ? activeClassName : undefined
-                        }><h3 className="md:text-xl">Profile</h3></NavLink>
+                        }><h3 className="md:text-base">Profile</h3></NavLink>
                     </div>
-                    <div className="flex justify-between gap-1 px-2 py-2 items-center cursor-pointer hover:text-blue-800 font-bold">
+                    <div className="flex justify-between gap-1 px-5 py-1 items-center cursor-pointer hover:bg-white hover:text-tara-0 font-bold">
                                 <NavLink to="/contact" className={({ isActive }) =>
                         isActive ? activeClassName : undefined
-                        }><h3 className="md:text-xl">Contact Us</h3></NavLink>
+                        }><h3 className="md:text-base">Contact Us</h3></NavLink>
                     </div>
                 </div>
         </div>
@@ -31,45 +31,42 @@ const SubNavbarSm = ()=>{
     );
 };
 
-const SubNavbarLg = ()=>{
-    return(
-        <>
-        <div className="px-3 w-full bg-tara-0 text-white py-2">
-            <div className="flex justify-center items-center gap-4">
-                <div className="flex justify-between gap-1 px-3 py-2 items-center cursor-pointer hover:bg-white hover:text-tara-0 hover:rounded-sm font-bold">
+// const SubNavbarLg = ()=>{
+//     return(
+//         <>
+//         <div className="px-3 w-full bg-tara-0 text-white py-2">
+//             <div className="flex justify-center items-center gap-4">
+//                 <div className="flex justify-between gap-1 px-3 py-2 items-center cursor-pointer hover:bg-white hover:text-tara-0 hover:rounded-sm font-bold">
                     
-                    <NavLink to="/" className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }><h3 className="text-sm">COURSES</h3></NavLink>
-                    {/* <BiChevronDown /> */}
-                </div>
-                <div className="flex justify-between gap-1 px-3 py-2 items-center cursor-pointer hover:bg-white hover:text-tara-0 hover:rounded-sm font-bold">
-                <NavLink to="/profile" className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }><h3 className="text-sm">PROFILE</h3></NavLink>
+//                     <NavLink to="/" className={({ isActive }) =>
+//               isActive ? activeClassName : undefined
+//             }><h3 className="text-sm">COURSES</h3></NavLink>
+//                     {/* <BiChevronDown /> */}
+//                 </div>
+//                 <div className="flex justify-between gap-1 px-3 py-2 items-center cursor-pointer hover:bg-white hover:text-tara-0 hover:rounded-sm font-bold">
+//                 <NavLink to="/profile" className={({ isActive }) =>
+//               isActive ? activeClassName : undefined
+//             }><h3 className="text-sm">PROFILE</h3></NavLink>
                     
-                    {/* <BiChevronDown /> */}
-                </div>
-                <div className="flex justify-between gap-1 px-3 py-2 items-center cursor-pointer hover:bg-white hover:text-tara-0 hover:rounded-sm font-bold">
-                <NavLink to="/contact" className={({ isActive }) =>
-              isActive ? activeClassName : undefined
-            }> <h3 className="text-sm">CONTACT US</h3></NavLink>
-                </div>
-            </div>
-        </div> 
-        </>
-    );
-};
+//                     {/* <BiChevronDown /> */}
+//                 </div>
+//                 <div className="flex justify-between gap-1 px-3 py-2 items-center cursor-pointer hover:bg-white hover:text-tara-0 hover:rounded-sm font-bold">
+//                 <NavLink to="/contact" className={({ isActive }) =>
+//               isActive ? activeClassName : undefined
+//             }> <h3 className="text-sm">CONTACT US</h3></NavLink>
+//                 </div>
+//             </div>
+//         </div> 
+//         </>
+//     );
+// };
 
 
 const SubNavbar = ()=>{
     return(
         <>
-            <div className="lg:hidden">
+            <div className="lg:hidden border-2">
                 <SubNavbarSm />
-            </div>
-            <div className="hidden lg:flex">
-                <SubNavbarLg />
             </div>
         </>
     );
